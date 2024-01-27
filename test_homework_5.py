836c8770989aaa082d976ab4ede0af3d2f732ded
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-01-27 16:25:04 +0300

    add final task

--- test_homework_5.py
+++ test_homework_5.py
@@ -79,17 +79,22 @@ def test_readable_function():
     go_to_companyname_homepage(page_url="https://companyname.com")
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
+def readable_function(func, *args, **kwargs):
+    f_name = func.__name__.replace('_', ' ').title()
+    f_arg = ", ".join([*args, *kwargs.values()])
+    res = f"{f_name} [{f_arg}]"
+    return res
 
-# def open_browser(browser_name):
-#     actual_result = None
-#     assert actual_result == "Open Browser [Chrome]"
-#
-#
-# def go_to_companyname_homepage(page_url):
-#     actual_result = None
-#     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
-#
-#
-# def find_registration_button_on_login_page(page_url, button_text):
-#     actual_result = None
-#     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+def open_browser(browser_name):
+    actual_result = readable_function(open_browser, browser_name=browser_name)
+    assert actual_result == "Open Browser [Chrome]"
+
+
+def go_to_companyname_homepage(page_url):
+    actual_result = readable_function(go_to_companyname_homepage, page_url)
+    assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    actual_result = readable_function(find_registration_button_on_login_page, page_url, button_text=button_text)
+    assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
